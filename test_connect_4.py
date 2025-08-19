be85761f1726912123d100506b3b93a26ced1706
Gijs ter Haar <gijs.terhaar@dataexpert.nl>
2025-08-19 13:48:30 +0200

    third_session: adding wide grid tests

--- test_connect_4.py
+++ test_connect_4.py
@@ -7,41 +7,54 @@ from connect_4 import (
 def test_target_in_sequence():
     assert connect_4([], 'red') == False
 
+
 def test_target_found():
     assert connect_4(['x', 'x', 'x', 'x'], 'x') == True
+
 
 def test_target_not_found_four_locations():
     assert connect_4(['x', 'y', 'x', 'x'], 'x') == False
 
+
 def test_target_found_in_seven():
     assert connect_4(['x', 'x', 'y','y','y','y','x'], 'y') == True
+
 
 def test_target_occurs_five_times():
     assert connect_4([1,1,1,1,1,], 1) == True
 
+
 def test_target_occurs_four_times_not_consecutively():
     assert connect_4([1,2,1,2,1,2,1], 1) == False
+
 
 def test_target_appears_zero_times():
     assert connect_4(['a','b'], 'c') == False
 
+
 def test_target_appears_one_time():
     assert connect_4(['a','b'], 'a') == False
+
 
 def test_target_appears_four_times_consecutive():
     assert connect_4([1,1,1,1,], '1') == False
 
+
 def test_get_row_empty_grid():
     assert get_row([], (1, 2)) == None
+
 
 def test_get_row_one_element_not_in_grid():
     assert get_row([['x']], (2, 1)) == None
 
+
 def test_get_row_one_element_in_grid():
     assert get_row([['x']], (0, 0)) == ['x']
 
+
 def test_get_row_two_elements_in_grid():
     assert get_row([['x', 'x']], (1, 0)) == ['x', 'x']
+
 
 def test_get_row_invalid_column():
     assert get_row(
@@ -51,25 +64,35 @@ def test_get_row_invalid_column():
         ],
         (4, 1)) == None
 
+
 def test_get_column_empty_grid():
     assert get_column([], (1, 2)) == None
+
 
 def test_get_column_one_element_in_grid():
     assert get_column([['x']], (0, 0)) == ['x']
 
+
 def test_get_column_two_elements_in_grid():
     assert get_column([['a', 'b'],
-                       ['c', 'd']
-],
+                       ['c', 'd']],
                        (1, 1)) == ['b', 'd']
+
+
 def test_get_column_one_element_not_in_grid():
     assert get_column([['a', 'b', 'c'],
                       ['d', 'e', 'f'],
                       ['g', 'h', 'i']],
-                      (4, 4)) == None
+                      (4, 4)) is None
+
 
 def test_get_column_wide_grid():
     assert get_column([['a', 'b', 'c', 'd'],
                        ['e', 'f', 'g', 'h']],
                       (3, 0)) == ['d', 'h']
 
+
+def test_get_column_not_in_wide_grit():
+    assert get_column([['a', 'b', 'c', 'd'],
+                       ['e', 'f', 'g', 'h']],
+                      (5, 0)) is None
